8a705f9dbab8e43cb267246a3b0040375cddc0cb
naimurnemu <naimurrahaman82@gmail.com>
2025-05-16 15:45:41 +0600

    connected mongoose to using mongodb database with server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+try {
+  mongoose.connect(MONGO_URL).then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  });
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error("MongoDB connection error:", error);
+}
